fba15415f92aef595253cb95ae4489ae379a1d8f
janvaljean <sfkrtld@gmail.com>
2022-12-06 13:54:42 +0100

    views contend added -more dynamic

--- blogapp/blog/urls.py
+++ blogapp/blog/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('' , views.index),
-    path('index', views.index),
-    path('blogs', views.blogs),
-    path('blogs/<int:id>', views.blog_details),
+    path('' , views.index, name="home"),
+    path('index', views.index ,name="home"),
+    path('blogs', views.blogs , name="blogs"),
+    path('blogs/<int:id>', views.blog_details, name="blog_details" ),
     
 ]
--- blogapp/blog/views.py
+++ blogapp/blog/views.py
@@ -1,13 +1,49 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
 
+
+data = {
+        "blogs" : [
+            {
+                "id" : 1 ,
+                "title" : "komple web gelistirme ",
+                "image": "1.jpg",
+                "is_active" : True,
+                "is_home": False,
+                "description" : "cok iyi bir kurs"
+                },
+            {
+                "id": 2,
+                "title": "Python Kursu ",
+                "image": "2.jpg",
+                "is_active": True,
+                "is_home": True,
+                "description": "cok iyi bir kurs"
+            },
+            {
+                "id": 3,
+                "title": "Django Kursu ",
+                "image": "3.jpg",
+                "is_active": False,
+                "is_home": True,
+                "description": "cok iyi bir kurs"
+            }
+
+        ]
+    }
 # Create your views here.
 def index(request):
-    return render(request, "blog/index.html")
+    context = {
+        "blogs" : data["blogs"]
+        }
+    return render(request, "blog/index.html", context)
 
 
 def blogs(request):
-    return render(request, "blog/blogs.html")
+    context = {
+        "blogs": data["blogs"]
+    }
+    return render(request, "blog/blogs.html", context)
 
 
 def blog_details(request, id):
