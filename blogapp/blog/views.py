c50e95d0b744e8c7fa817a29dcc13ef2c781a344
janvaljean <sfkrtld@gmail.com>
2022-12-07 22:11:10 +0100

    models, migrate changed. Database added

--- blogapp/blog/views.py
+++ blogapp/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
+from blog.models import Blog
 
 
 data = {
@@ -34,17 +35,18 @@ data = {
 # Create your views here.
 def index(request):
     context = {
-        "blogs" : data["blogs"]
+        "blogs": Blog.objects.filter(is_home=True, is_active=True)
         }
     return render(request, "blog/index.html", context)
 
 
 def blogs(request):
     context = {
-        "blogs": data["blogs"]
+        "blogs": Blog.objects.filter(is_active=True)
     }
     return render(request, "blog/blogs.html", context)
 
 
 def blog_details(request, id):
-    return render(request, "blog/blog-details.html", {"id" : id})
+    blog = Blog.objects.get(id=id)      
+    return render(request, "blog/blog-details.html", {"blog" : blog})
